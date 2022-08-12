609af5718bd0865f0080fcb475c4cf3fbd2f1eb8
Dima Shirkov <dimashirkov@list.ru>
2022-08-12 11:26:31 +0300

    dak1 draft v3

--- task.js
+++ task.js
@@ -2,14 +2,29 @@ const pollTitle = document.getElementById('poll__title'); // доступ к в�
 const pollAnswers = document.getElementById('poll__answers'); // обертка для ответов
 const pollAnswer = document.createElement('button');
 pollAnswer.classList.add('poll__answer');
-
+let result;
+let index;
+let respNumber;
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
-xhr.send();
 
-xhr.onload = function() {
+function serverRequest(arg) {
+    if (arg === 'get') {
+        xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+        xhr.send();
+        xhr.addEventListener('load', questionLoading);
+    } else if (arg === respNumber) {
+        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send(`vote=${index}&answer=${respNumber}`);
+
+        xhr.onload = function() {
+            console.log(JSON.parse(xhr.response));
+        }
+    }
+}
+
+function questionLoading() {
     let result = JSON.parse(xhr.response);
-    let index;
 
     Object.entries(result).forEach(([key, value]) => {
         if (key === 'id') {
@@ -17,7 +32,6 @@ xhr.onload = function() {
         }
         
         Object.entries(value).forEach(([nastedKey, nastedValue]) => {
-            console.log(nastedKey, nastedValue);
             if (nastedKey === 'title') {
                 pollTitle.textContent = nastedValue;
             } else {
@@ -33,17 +47,56 @@ xhr.onload = function() {
     document.querySelectorAll('.poll__answer').forEach((answer, responseNumber) => {
         answer.addEventListener('click', () => {
             alert('Спасибо, ваш голос засчитан!');
-            
-            xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
-            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            xhr.send(`vote=${index}&answer=${responseNumber}`);
-
-            xhr.onload = function() {
-                console.log(JSON.parse(xhr.response));
-            }
+            respNumber = responseNumber;
+            serverRequest(respNumber);
         })
     })
-   
 }
+
+serverRequest('get');
+
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+// xhr.send();
+
+
+// xhr.onload = function() {
+//     let result = JSON.parse(xhr.response);
+//     let index;
+
+//     Object.entries(result).forEach(([key, value]) => {
+//         if (key === 'id') {
+//             index = value;
+//         }
+        
+//         Object.entries(value).forEach(([nastedKey, nastedValue]) => {
+//             console.log(nastedKey, nastedValue);
+//             if (nastedKey === 'title') {
+//                 pollTitle.textContent = nastedValue;
+//             } else {
+//                 nastedValue.forEach((answer) => {
+//                     let pollAnswerClone = pollAnswer.cloneNode(false);
+//                     pollAnswerClone.textContent = answer;
+//                     pollAnswers.appendChild(pollAnswerClone);
+//                 })
+//             }
+//         })
+//     })
+
+//     document.querySelectorAll('.poll__answer').forEach((answer, responseNumber) => {
+//         answer.addEventListener('click', () => {
+//             alert('Спасибо, ваш голос засчитан!');
+            
+//             xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
+//             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//             xhr.send(`vote=${index}&answer=${responseNumber}`);
+
+//             xhr.onload = function() {
+//                 console.log(JSON.parse(xhr.response));
+//             }
+//         })
+//     })
+   
+// }
 
 
