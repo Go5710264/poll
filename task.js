31efb61cf634d333b259655ddf31a3e1ba3ed78d
Dima Shirkov <dimashirkov@list.ru>
2022-08-17 20:11:04 +0300

    dak1 draft v6

--- task.js
+++ task.js
@@ -73,48 +73,4 @@ function questionLoading() {
 
 serverRequest('get');
 
-// const xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
-// xhr.send();
-
-
-// xhr.onload = function() {
-//     let result = JSON.parse(xhr.response);
-//     let index;
-
-//     Object.entries(result).forEach(([key, value]) => {
-//         if (key === 'id') {
-//             index = value;
-//         }
-        
-//         Object.entries(value).forEach(([nastedKey, nastedValue]) => {
-//             console.log(nastedKey, nastedValue);
-//             if (nastedKey === 'title') {
-//                 pollTitle.textContent = nastedValue;
-//             } else {
-//                 nastedValue.forEach((answer) => {
-//                     let pollAnswerClone = pollAnswer.cloneNode(false);
-//                     pollAnswerClone.textContent = answer;
-//                     pollAnswers.appendChild(pollAnswerClone);
-//                 })
-//             }
-//         })
-//     })
-
-//     document.querySelectorAll('.poll__answer').forEach((answer, responseNumber) => {
-//         answer.addEventListener('click', () => {
-//             alert('Спасибо, ваш голос засчитан!');
-            
-//             xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
-//             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//             xhr.send(`vote=${index}&answer=${responseNumber}`);
-
-//             xhr.onload = function() {
-//                 console.log(JSON.parse(xhr.response));
-//             }
-//         })
-//     })
-   
-// }
-
 
